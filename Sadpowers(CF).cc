efb261ebea49f2a8e5af6f655a42e2efa169c7e2
YazidFt <ysimofox@gmail.com>
2018-03-30 00:59:54 +0100

    Update Sadpowers(CF).cc

--- Sadpowers(CF).cc
+++ Sadpowers(CF).cc
@@ -2,7 +2,7 @@
 Link: http://codeforces.com/contest/955/problem/C
 
 Given Q queries of the form (L, R)
-For each query you have to find the number of such x that ( L ≤ x ≤ R ) and there exist integer numbers a > 0, p > 1 such that x = power(a, p).
+For each query you have to find the number of such x that (L ≤ x ≤ R) and there exist integer numbers a>0, p>1 such that x = power(a, p).
 
 */
 
@@ -29,13 +29,13 @@ const ll mx = 1e18 + 18;
 
   void precalc(){
          for(int i = 2; i <= 1e6; i++ ){
-					ll r = i;
-          while(r < mx && (log10(r)+log10(i) < log10(mx))){
+	    ll r = i;
+            while(r < mx && (log10(r)+log10(i) < log10(mx))){
                   r = r*i;
-						      ll sq = sqrt(r);
-						      if(sq*sq != r) v.pb(r);
-					     }
-			     }	
+		  ll sq = sqrt(r);
+		  if(sq*sq != r) v.pb(r);
+		}
+	    }	
         }
 
 
@@ -55,10 +55,10 @@ int main(){
       ll l, r, res;
       cin >> l >> r;
 
-	    res = (upper_bound(v.begin(), v.end(), r) - v.begin()) - (upper_bound(v.begin(), v.end(), l-1) - v.begin());
+      res = (upper_bound(v.begin(), v.end(), r) - v.begin()) - (upper_bound(v.begin(), v.end(), l-1) - v.begin());
       res += (ll)sqrt(r) - (ll)sqrt(l-1);
 
-	    cout << res << endl;
+      cout << res << endl;
     }
 
   
